673fac053c1d87fc5003f28279d667c0ed6062ca
Poncho <lescouarneckevin@yahoo.fr>
2017-01-11 22:43:15 +0100

    Add dist to github

---
+++ dist/app/appRoute.js
@@ -0,0 +1,31 @@
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('home', {
+    url: '/home',
+    views:{
+     'home':{
+      templateUrl: 'app/views/home.html',
+      controller: 'homeCtrl'
+     }
+    }
+  })
+  .state('player', {
+    url: '/player',
+    views:{
+     'home':{
+      templateUrl: 'app/views/player.html',
+      controller: 'playerCtrl'
+     }
+    }
+  })
+  .state('contact', {
+    url: '/contact',
+    views:{
+     'home':{
+      templateUrl: 'app/views/contact.html',
+      controller: 'contactCtrl'
+     }
+    }
+  })
+  $urlRouterProvider.otherwise('/home');
+});
---
+++ dist/app/js/controllers/contactCtrl.js
@@ -0,0 +1,2 @@
+app.controller('contactCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+}]);
